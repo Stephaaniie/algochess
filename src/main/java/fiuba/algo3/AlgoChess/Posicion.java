602c1c48d522eb2995c9e8a6acad356279d4c1b5
Agustina Fraccaro <afraccaro@fi.uba.ar>
2019-11-18 16:59:36 -0300

    agregue el metodo equals en posicion

--- src/main/java/fiuba/algo3/AlgoChess/Posicion.java
+++ src/main/java/fiuba/algo3/AlgoChess/Posicion.java
@@ -2,7 +2,7 @@ package fiuba.algo3.AlgoChess;
 
 import fiuba.algo3.AlgoChess.teclado.Direccion;
 
-public class Posicion {
+public class Posicion{
 	
 	public int fila;
 
@@ -48,4 +48,17 @@ public class Posicion {
 
 		return distancia;
 	}
+	
+	   @Override
+	    public boolean equals(Object obj) {
+	        if (!(obj instanceof Posicion)) {
+	            return false;
+	        }
+	        if (obj == this) {
+	            return true;
+	        }
+	        Posicion otro = (Posicion) obj;
+
+	        return (this.getFila() == otro.getFila() && this.getColumna() == otro.getColumna());
+	   }
 }
