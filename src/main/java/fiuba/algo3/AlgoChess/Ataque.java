a1cd33bbe6d68318c125e8bf9cf3cf7fe294b62d
Stephanie Castillo <stephaanie.castillo@gmail.com>
2019-11-18 20:02:29 -0300

    Actualizando rama

--- src/main/java/fiuba/algo3/AlgoChess/Ataque.java
+++ src/main/java/fiuba/algo3/AlgoChess/Ataque.java
@@ -2,8 +2,7 @@ package fiuba.algo3.AlgoChess;
 
 public interface Ataque {
 
-		//Le puse entidad Movil porque CAmi aun no pudo subir la entidad pero deberia recibir dos entedidades
-		//
+		
 	public void atacarEnemigo(EntidadMovil ofensivo);
 
 }
