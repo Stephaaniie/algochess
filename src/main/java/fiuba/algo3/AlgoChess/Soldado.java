24a6166fcdd99f8d7ff13ad41fff4be6e2a20409
Agustina Schmidt <agustina.schmidt@hotmail.com>
2019-11-19 16:29:19 -0300

    arreglo conflictos

--- src/main/java/fiuba/algo3/AlgoChess/Soldado.java
+++ src/main/java/fiuba/algo3/AlgoChess/Soldado.java
@@ -10,7 +10,7 @@ public class Soldado implements EntidadMovil {
 	private int vida = VIDAINICIAL;
 	private int costo = 1;
 	private Posicion posicion;
-/*
+
 	public Soldado(String bando, int fila, int columna) {
 		this.bando = bando;
 		this.posicion = new Posicion(fila, columna);
@@ -18,15 +18,6 @@ public class Soldado implements EntidadMovil {
 
 	public int getVida() {
 		return this.vida;
-	}
-
-	@Override
-	public void atacarEnemigo(Entidad entidadAtacada) {
-		// ArrayList<Object> enemigos = new ArrayList<Object>();
-		// enemigos = calculadorDistancia.distanciaCercana(posicion);
-
-		// enemigos.forEach(enemigo -> enemigo.restarVida(this.danioACuerpo));
-
 	}
 
 	@Override
@@ -60,60 +51,11 @@ public class Soldado implements EntidadMovil {
 
 		}
 	}
-/*
-	@Override
-	public void reponerVida(int curacion) throws CuranderoCuraHastaLaMaximaVidaExcepcion{
-
-		if ((this.vida += curacion) > VIDAINICIAL){
-
-			this.vida = VIDAINICIAL;
-
-			new CuranderoCuraHastaLaMaximaVidaExcepcion("El curandero ya tiene el valor maximo de vida");
-
-		}
-	}
-
-*/
-/*
-	@Override
-	public void reponerVida(int curacion) throws CuranderoCuraHastaLaMaximaVidaExcepcion {
-		// TODO Auto-generated method stub
-		
-	}*/
-
-	public void atacarEnemigo1(Entidad entidadAtacada) throws ObjetoNuloNoPuedeRealizarNingunaAccionExcepcion {
-	}
-
-	@Override
-	public boolean aliadosCerca() throws ObjetoNuloNoPuedeRealizarNingunaAccionExcepcion {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean enemigosCerca() throws ObjetoNuloNoPuedeRealizarNingunaAccionExcepcion {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public void recibirDanio(int danio) throws ObjetoNuloNoPuedeRealizarNingunaAccionExcepcion {
-		// TODO Auto-generated method stub
-		
-	}
 
 	@Override
 	public void atacarEnemigo(Entidad entidadAtacada) throws ObjetoNuloNoPuedeRealizarNingunaAccionExcepcion {
-		// TODO Auto-generated method stub
-		
+		entidadAtacada.recibirDanio(this.danioACuerpo);
 	}
+		
 
-	@Override
-	public void reponerVida(int curacion) throws ObjetoNuloNoPuedeRealizarNingunaAccionExcepcion {
-		// TODO Auto-generated method stub
-		
-	}
-		
-		
-	
 }
--- src/test/java/fiuba/algo3/AlgoChess/SoldadoTest.java
+++ src/test/java/fiuba/algo3/AlgoChess/SoldadoTest.java
@@ -1,46 +1,47 @@
 package fiuba.algo3.AlgoChess;
+import fiuba.algo3.AlgoChess.excepciones.CuranderoCuraHastaLaMaximaVidaExcepcion;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class SoldadoTest {
-	
+
 	@Test
-	public void soldadoRecuperadoSeLeSumaVidaTest() {
-		/*			throws CuranderoCuraHastaLaMaximaVidaExcepcion {
-		Casillero casillero = new Casillero(1,2);
-		Soldado soldado = new Soldado("aliado", casillero);
+	public void soldadoRecuperadoSeLeSumaVidaTest()
+			throws CuranderoCuraHastaLaMaximaVidaExcepcion {
+		Soldado soldado = new Soldado("aliado", 1, 2);
 		soldado.reponerVida(15);
-			
+
 		Assert.assertEquals(100, soldado.getVida());
 	}
-	
-	@Test 
+
+	@Test
 	public void soldadoAtacadoPierdeVidaTest() {
-		Casillero casillero = new Casillero(1,2);
-		Soldado soldado = new Soldado("aliado", casillero);
-		
+		Soldado soldado = new Soldado("aliado", 1, 2);
+
 		soldado.recibirDanio(50);
 
 		Assert.assertEquals(50, soldado.getVida());
 	}
-	
+
 	@Test
 	public void soldadoNoTieneEnemigosCercaTest() {
-		Casillero casillero = new Casillero(3,5);
-		Soldado soldado = new Soldado("aliado", casillero);
-		
-		Assert.assertEquals(false, soldado.enemigosCerca());
+		Soldado soldado = new Soldado("aliado", 3, 5);
+
+		Assert.assertFalse(soldado.enemigosCerca());
 	}
-	
+
 	@Test
 	public void soldadoNoTieneAliadosCercaTest() {
-		Casillero casillero = new Casillero(6,7);
-		Soldado soldado = new Soldado("enemigo", casillero);
-		
-		Assert.assertEquals(false, soldado.aliadosCerca());
-		*/
-	}
-	
-	
-	
+		Soldado soldado = new Soldado("enemigo", 6, 7);
 
+		Assert.assertFalse(soldado.aliadosCerca());
+
+	}
+
+	@Test
+	public void soldadoAtacaACuranderoYLeRestaVida(){
+		Soldado soldado = new Soldado("enemigo", 6, 7);
+		//Curandero curandero = new Curandero("enemigo", );
+		//soldado.atacarEnemigo();
+	}
 }
