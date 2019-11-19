ac851c17a7d019371df810fa152cf3736382e8b3
camila <camicaserta@hotmail.com>
2019-11-19 18:14:04 -0300

    cambios en clase Direccion

--- src/main/java/fiuba/algo3/AlgoChess/teclado/Direccion.java
+++ src/main/java/fiuba/algo3/AlgoChess/teclado/Direccion.java
@@ -4,57 +4,31 @@ import fiuba.algo3.AlgoChess.Posicion;
 
 public class Direccion {
 
-	private int fila;
-	private int columna;
+    public Posicion arriba(Posicion posicion) {
+    	
+        int filaDeArriba = posicion.getFila() - 1;
 
-    private static Direccion arriba;
-    private static Direccion abajo;
-    private static Direccion derecha;
-    private static Direccion izquierda;
+        return new Posicion(filaDeArriba, posicion.getColumna());
+    }
 
-    static {
-        arriba    = new Direccion(0,1);
-        abajo     = new Direccion(0,-1);
-        derecha   = new Direccion(1,0);
-        izquierda = new Direccion(-1,0);
+    public Posicion abajo(Posicion posicion) {
+    	
+        int filaDeAbajo = posicion.getFila() + 1;
+
+        return new Posicion(filaDeAbajo, posicion.getColumna());
     }
     
-    Direccion(int fila, int columna) {
+    public Posicion derecha(Posicion posicion) {
     	
-    	this.fila = fila;
-    	this.columna = columna;
-    }
-
-	public static Direccion abajo() {
-
-        return abajo();
-    }
-
-    public static Direccion arriba() {
-
-        return arriba();
-    }
-
-    public static Direccion derecha() {
-        return derecha();
-    }
-    
-    public static Direccion izquierda() {
-    	return izquierda();
-    }
-    public Posicion siguiente(Posicion posicion) {
-    	
-        int siguienteFila = posicion.getFila() + 1;
         int siguienteColumna = posicion.getColumna() + 1;
 
-        return new Posicion(siguienteFila, siguienteColumna);
+        return new Posicion(posicion.getFila(), siguienteColumna);
     }
-
-    public Posicion anterior(Posicion posicion) {
+    
+    public Posicion izquierda(Posicion posicion) {
     	
-        int siguienteFila = posicion.getFila() - 1;
-        int siguienteColumna = posicion.getColumna() - 1;
+        int columnaAnterior = posicion.getColumna() - 1;
 
-        return new Posicion(siguienteFila, siguienteColumna);
+        return new Posicion(posicion.getFila(), siguienteColumna);
     }
 }
