cb7657f2307485bb4f4ac1b33c253ebb4f953947
Stephanie Castillo <stephaanie.castillo@gmail.com>
2019-11-19 16:16:16 -0300

    test para curandero

--- src/main/java/fiuba/algo3/AlgoChess/Entidad.java
+++ src/main/java/fiuba/algo3/AlgoChess/Entidad.java
@@ -1,13 +1,12 @@
 package fiuba.algo3.AlgoChess;
 
 import fiuba.algo3.AlgoChess.excepciones.CuranderoCuraHastaLaMaximaVidaExcepcion;
-import fiuba.algo3.AlgoChess.excepciones.CuranderoNoCuraAunaEntidadFija;
 import fiuba.algo3.AlgoChess.excepciones.ObjetoNuloNoPuedeRealizarNingunaAccionExcepcion;
 
 public interface Entidad {
 
 	public void recibirDanio(int danio) throws ObjetoNuloNoPuedeRealizarNingunaAccionExcepcion;
 
-	public void reponerVida(int curacion) throws ObjetoNuloNoPuedeRealizarNingunaAccionExcepcion, CuranderoCuraHastaLaMaximaVidaExcepcion, CuranderoNoCuraAunaEntidadFija;
+	public void reponerVida(int curacion) throws ObjetoNuloNoPuedeRealizarNingunaAccionExcepcion, CuranderoCuraHastaLaMaximaVidaExcepcion;
 
 }
